a626597b2737deda95787034eed4b8c9f4c13707
thonukunuridinesh-eng <thonukunuridinesh@gmail.com>
2026-05-28 13:28:09 +0530

    Auto-create database tables on startup

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-from app import create_app, db, socketio
+from app import create_app, db
 
 app = create_app()
 
+# Add this block to create tables in the cloud database automatically
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+if __name__ == "__main__":
+    app.run()
